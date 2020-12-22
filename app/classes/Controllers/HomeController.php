98965ce1853e47fd10bf1f22ff407e1b385f810c
laurynasg <glaurynasg@gmail.com>
2020-12-22 22:38:18 +0200

    Edit HomePage

--- app/classes/Controllers/HomeController.php
+++ app/classes/Controllers/HomeController.php
@@ -24,6 +24,23 @@ class HomeController
 
         $content = (new View([
             'heading' => 'This is the HomePage',
+            'services' => [
+                [
+                    'img' => 'service1 img',
+                    'name' => 'service1 name here',
+                    'description' => 'service1 description'
+                ],
+                [
+                    'img' => 'service2 img',
+                    'name' => 'service2 name here',
+                    'description' => 'service2 description'
+                ],
+                [
+                    'img' => 'service3 img',
+                    'name' => 'service3 name here',
+                    'description' => 'service3 description'
+                ],
+            ]
         ]))->render(ROOT . '/app/templates/index.tpl.php');
 
         $this->page->setContent($content);
